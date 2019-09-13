988ee665c6ce4074ae33478a9ac4380cb9cea03e
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-09-13 00:46:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,8 @@ def monopoly_with_third_tier
         :reading_railroad =>{},
         :pennsylvania_railroad =>{},
         :b_and_o_railroad =>{},
-        :shortline =>{}
+        :shortline =>{},
+        
         
       } , 
         :rent_in_dollars => {
